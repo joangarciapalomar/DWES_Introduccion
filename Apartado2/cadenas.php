f0da6914a1005d343317ab7a095830931f538081
joangarciapalomar <garciapalomar.j14@gmail.com>
2023-09-15 10:19:16 +0200

    Mi primer commit

--- Apartado2/cadenas.php
+++ Apartado2/cadenas.php
@@ -1,47 +1,53 @@
 <?php 
-    $nombre = trim($_GET['nombre'] ?? 1, "/");
-    
-    $prefijo = ($_GET["prefijo"] ?? 1);
+$nombre = trim($_GET['nombre'] ?? '', "/");
 
-    $len = strlen ($nombre);
-    
-    $mayus = strtoupper($nombre);
-    
-    $minus = strtolower($nombre);
+$prefijo = ($_GET["prefijo"] ?? '');
 
-    $pos = strpos($nombre, $prefijo);
+$len = strlen($nombre);
 
-    $countA = substr_count($minus, "a") ;
+$mayus = strtoupper($nombre);
 
-    $posA = stripos($nombre, "a") + 1 ?? 1;
+$minus = strtolower($nombre);
 
-    $sustO = str_ireplace("o", "0", $nombre);
-   
-    echo "Tu nombre es: $nombre, 
-    <br>Tu cadena mide $len caracteres
-    <br>Tu nombre en mayúsculas es: $mayus 
-    <br>Tu nombre en minúsculas es: $minus";
-    
-    if ($prefijo != 1){
-        if ($pos === false) {
+$pos = strpos($nombre, $prefijo);
 
-            echo "<br>El prefijo '$prefijo' no fue encontrado en su nombre '$nombre'";
-        } else {
+$countA = substr_count($minus, "a");
 
-            echo "<br>El prefijo '$prefijo' fue encontrado en su nombre '$nombre'";
-        }
-    }else{
+$posA = stripos($nombre, "a");
 
-        echo "<br>Inserta un Prefijo para esta función";
+$sustO = str_ireplace("o", "0", $nombre);
+
+$url = 'http://username:password@hostname:9090/path?arg=value';
+
+echo "Tu nombre es: $nombre, 
+<br>Tu cadena mide $len caracteres
+<br>Tu nombre en mayúsculas es: $mayus 
+<br>Tu nombre en minúsculas es: $minus";
+
+if (!empty($prefijo)){
+    if ($pos === false) {
+        echo "<br>El prefijo '$prefijo' no fue encontrado en su nombre '$nombre'";
+    } else {
+        echo "<br>El prefijo '$prefijo' fue encontrado en su nombre '$nombre'";
     }
+}else{
+    echo "<br>Inserta un Prefijo para esta función";
+}
 
-    echo "<br>Hay un total de $countA de caracteres 'a o A'";
+echo "<br>Hay un total de $countA de caracteres 'a o A'";
 
-    if ($posA === 1){
-        echo "<br>No existe ninguna a";
-    }else{
-        echo "<br>La posición de la primera a és : $posA ";
-    }
+if ($posA === false){
+    echo "<br>No existe ninguna 'a' en tu nombre";
+}else{
+    echo "<br>La posición de la primera 'a' es: " . ($posA + 1);
+}
 
-    echo "<br>Tu nombre cambiando las 'o' por ceros: $sustO";
+echo "<br>Tu nombre cambiando las 'o' por ceros: $sustO";
+
+echo "<br><br>Siendo la URL la siguiente: 'http://username:password@hostname:9090/path?arg=value';";
+
+echo "<br><br>El protocolo sería: " . parse_url($url, PHP_URL_SCHEME);
+echo "<br>El usuario sería: " . parse_url($url, PHP_URL_USER);
+echo "<br>El path sería: " . parse_url($url, PHP_URL_PATH);
+echo "<br>Por último, el query sería: " . parse_url($url, PHP_URL_QUERY);
 ?>
